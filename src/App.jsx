ee54d40a62c0f8d8fec0e59c2f01d453b6599aa2
rex0315 <97576382+rex0315@users.noreply.github.com>
2023-09-26 15:15:16 +0800

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
+const API_URL = import.meta.env.VITE_API_URL;
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [excuse, setExcuse] = useState("");
+  const [choice, setChoice] = useState("");
+
+  const fetchExcuse = async (choice) => {
+    await axios
+      .get(`https://jsonplaceholder.typicode.com/${choice}`)
+      .then((response) => {
+        setExcuse(response.data);
+        console.log(response.data);
+      });
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <button className="todos" onClick={() => fetchExcuse("todos")}>
+        Todos
+      </button>
+      <button className="todos" onClick={() => fetchExcuse("posts")}>
+        Todos
+      </button>
+      {/* {excuse.map((item, index) => (
+        <div>{<p key={index}>{item.title} </p>}</div>
+      ))} */}
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/services/APIService.js
@@ -0,0 +1,9 @@
+import axios from 'axios'
+
+const API_URL = import.meta.env.VITE_API_URL;
+
+export const APIService = async ({ choose }) => {
+
+}
+
+export default APIService
