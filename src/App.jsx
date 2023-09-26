89cd90f63d9328179fa5662762bea540c842b12c
Rex Batongbakal <memoxpress.rex@gmail.com>
2023-09-26 23:12:32 +0800

    redux sample

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,24 @@
 import "./App.css";
-import { useEffect, useState } from "react";
-import axios from "axios";
-const API_URL = import.meta.env.VITE_API_URL;
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NavBar from "./Components/NavBar";
+import { Home } from "./Pages/Home";
+import { About } from "./Pages/About";
+import { Contact } from "./Pages/Contact";
+import { Provider, store } from "react-redux";
 
 function App() {
-  const [excuse, setExcuse] = useState("");
-  const [choice, setChoice] = useState("");
-
-  const fetchExcuse = async (choice) => {
-    await axios
-      .get(`https://jsonplaceholder.typicode.com/${choice}`)
-      .then((response) => {
-        setExcuse(response.data);
-        console.log(response.data);
-      });
-  };
-
   return (
     <div>
-      <button className="todos" onClick={() => fetchExcuse("todos")}>
-        Todos
-      </button>
-      <button className="todos" onClick={() => fetchExcuse("posts")}>
-        Todos
-      </button>
-      {/* {excuse.map((item, index) => (
-        <div>{<p key={index}>{item.title} </p>}</div>
-      ))} */}
+      <Provider store={store}>
+        <Router>
+          <NavBar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/About" element={<About />} />
+            <Route path="/Contact" element={<Contact />} />
+          </Routes>
+        </Router>
+      </Provider>
     </div>
   );
 }
